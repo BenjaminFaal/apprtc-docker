a617c4608e454fdebda28e3966fd90fe3b0d1f39
BenjaminFaal <benjamin_faal@hotmail.com>
2016-06-27 11:38:29 +0200

    changed cmd to entrypoint for ENV variabls

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,11 @@
-test
+sed -i 's/SHARED_KEY_REPLACE/'"$SHARED_KEY"'/g' apprtc/src/app_engine/apprtc.py
+sed -i 's/TURN_IP_REPLACE/'"$TURN_IP"'/g' apprtc/src/app_engine/apprtc.py
+sed -i 's/TURN_PORT_REPLACE/'"$TURN_PORT"'/g' apprtc/src/app_engine/apprtc.py
+
+npm install -g npm
+npm install -g grunt-cli
+
+npm install
+grunt build
+
+exec "$@"
